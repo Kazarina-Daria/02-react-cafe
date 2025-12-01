a786059eb43826cd6a8f2456134bf081e3063a5f
Daria Kazarina <kdasa033@gmail.com>
2025-12-01 19:20:39 +0100

    changes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo";
 import type { VoteType } from "../../types/votes";
-import type Vote from "../../types/votes";
+import type {Votes} from "../../types/votes";
 import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../VoteStats/VoteStats";
 import Notification from "../Notification/Notification";
 
 export default function App() {
-  const [votes, setVotes] = useState<Vote>({ good: 0, neutral: 0, bad: 0 });
+  const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
 
   function handleVote(option: VoteType) {
     setVotes((prev) => ({
